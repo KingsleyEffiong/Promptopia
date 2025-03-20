91b6671654ddd2d4751e1476edfcc06149636748
kingsleyeffiong <kingsleyeffiong642@gmail.com>
2025-03-20 10:12:17 +0100

    updated

--- components/Form.jsx
+++ components/Form.jsx
@@ -25,7 +25,14 @@ function Form({ type,
                 </label>
                 <div className="flex-end mx-3 mb-5 gap-4">
                     <Link href='/' className="text-gray-500">Cancle</Link>
-                    <button type="submit" disabled={submitting} className="px-5 py-1.5 text-sm bg-orange-600 rounded-full text-white">{submitting ? `${type}....` : type}</button>
+                    <button
+                        type="submit"
+                        disabled={submitting}
+                        className="px-5 py-1.5 text-sm bg-orange-600 rounded-full text-white"
+                    >
+                        {submitting ? `${type === "Create" ? "Creating" : "Editing"}....` : type}
+                    </button>
+
                 </div>
             </form>
         </section>
